b81978b697b5b11c893bc5e3c92ddebaeb169fcb
Abreham Nuradis <abrehamnuradis222@gmail.com>
2024-12-10 00:37:16 +0300

    Event

--- src/components/Students/Events.jsx
+++ src/components/Students/Events.jsx
@@ -21,7 +21,7 @@ const Events = () => {
       {/* Calendar & Filtered Events Section */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {/* Calendar */}
-        <div className="bg-blue-50 p-4 rounded-lg shadow-md">
+        <div className="bg-blue-50 p-4 rounded-lg shadow-md flex justify-center">
           <Calendar
             onChange={setSelectedDate}
             value={selectedDate}
