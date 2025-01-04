d0249a9718bf8abc9dee33157488a52cdc8c3697
Abreham Nuradis <abrehamnuradis222@gmail.com>
2025-01-04 15:00:30 +0300

    NOt much change

--- src/Layouts/StudentLayout.jsx
+++ src/Layouts/StudentLayout.jsx
@@ -8,8 +8,8 @@ const StudentLayout = () => {
   const [showProfile, setShowProfile] = useState(false);
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Topbar showProfile={setShowProfile} />
+    <div className="grid grid-cols-2">
+      <Topbar showProfile={setShowProfile} className='' />
       {showProfile && <ProfileDetail />}
       <div className="flex flex-1 bg-gradient-to-r from-blue-100 to-gray-100" >
         <Aside />
